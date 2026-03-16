cfae1d0dd11bac4e2fc69f94f068794f3bc654f2
gallardo-0000 <darlenygallardo@gmail.com>
2026-03-16 09:46:41 -0600

    Update RainSimulation.h

--- src/RainSimulation.h
+++ src/RainSimulation.h
@@ -2,26 +2,32 @@
 #include <vector>
 
 class RainSimulation {
+
 public:
+
     void setup(int w, int h);
-    void update(const std::vector<float>& terrainHeights);
+
+    void update(const std::vector<float>& terrain);
 
     void addRain(float amount);
+
     void addLocalizedRain(int cx, int cy, float radius, float amount);
+
     void clearWater();
 
     const std::vector<float>& getWater() const;
 
 private:
-    int width = 0;
-    int height = 0;
 
-    std::vector<float> terrain;
+    int width;
+    int height;
+
+    std::vector<float> terrainHeight;
     std::vector<float> water;
     std::vector<float> nextWater;
 
-    float infiltrationRate = 0.001f;
-    float flowRate = 0.15f;
+    float infiltration = 0.001f;
+    float flowSpeed = 0.2f;
 
     int index(int x, int y) const;
 };
