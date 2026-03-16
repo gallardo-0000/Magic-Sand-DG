64e84ebfd41937fa3e65d4fd765e2c52aeb1dc47
gallardo-0000 <darlenygallardo@gmail.com>
2026-03-16 10:11:51 -0600

    ofApp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -32,30 +32,25 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 class ofApp : public ofBaseApp {
 
 public:
-	void setup();
- 
-	void update();
+    void setup();
+    void update();
+    void draw();
+    void drawProjWindow(ofEventArgs& args);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-	void draw();
-	void drawProjWindow(ofEventArgs& args);
-
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-
-	std::shared_ptr<ofAppBaseWindow> projWindow;
+    std::shared_ptr<ofAppBaseWindow> projWindow;
 
 private:
-
-    // SIMULACIÓN DE LLUVIA
     RainSimulation rainSim;
 
     bool rainActive = false;
@@ -74,7 +69,6 @@ private:
     SandSurfaceRenderer* sandSurfaceRenderer;
     CMapGameController mapGameController;
     CBoidGameController boidGameController;
-
     // Main window ROI
     ofRectangle mainWindowROI;
 };
